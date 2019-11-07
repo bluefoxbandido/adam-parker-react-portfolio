bd876ded98171ddbbf42b444553fe1291c91c5f1
bluefoxbandido <adamthewonderboy@gmail.com>
2019-11-07 12:58:53 -0600

    Finalized Initial Commit

---
+++ src/components/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import moment from "moment";
+
+import PortfolioContainer from './portfolio-container';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>Developer Portfolio</h1>
+        <PortfolioContainer/>
+        <div>
+          
