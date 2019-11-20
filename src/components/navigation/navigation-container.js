848bdaf304a93e73390c8613f579d5ec03f1e840
bluefoxbandido <adamthewonderboy@gmail.com>
2019-11-20 15:28:38 -0600

    dropzone component

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -8,7 +8,7 @@ const NavigationComponent = props => {
     const dynamicLink = (route, linkText) => {
         return (
             <div className="nav-link-wrapper">
-                <NavLink to="/blog" activeClassName="nav-link-active">Blog</NavLink>
+                <NavLink to={route} activeClassName="nav-link-active">{linkText}</NavLink>
             </div>
         )
     }
@@ -38,7 +38,10 @@ const NavigationComponent = props => {
                 <div className="nav-link-wrapper">
                     <NavLink to="/contact" activeClassName="nav-link-active">Contact</NavLink>
                 </div>
-                {props.loggedInStatus === "LOGGED_IN" ? dynamicLink("/blog", "Blog") : null}
+                <div className="nav-link-wrapper">
+                    <NavLink to="/blog" activeClassName="nav-link-active">Blog</NavLink>
+                </div>
+                {props.loggedInStatus === "LOGGED_IN" ? dynamicLink("/portfolio-manager", "Portfolio Manager") : null}
             </div>
             <div className="right-side">
                 Adam Parker
